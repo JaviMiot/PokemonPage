973ecb87685d0fcace6a0b0632ee3cb3dc8db30b
Javier Manobanda <51manobandajavier@gmail.com>
2021-10-19 11:48:12 -0500

    update eslint

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -50,12 +50,3 @@ export const getStaticPaths = async () => {
   const paths = [{ params: { id: '1' } }];
   return { paths, fallback: true };
 };
-
-/* export const getServerSideProps = async ({ params }) => {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${params.id}`
-  );
-  const data = await response.json();
-  return { props: { data } };
-};
- */
